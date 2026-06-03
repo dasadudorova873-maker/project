a58145787db05649a20bfcb28b3090fc4cfd48e8
dasadudorova873-maker <dasadudorova873@gmail.com>
2026-06-03 13:49:26 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const closeModalButton=document.querySelector(".application_close");
 closeModalButton.addEventListener("click", function(event){
     modalApplication.setAttribute("hidden", true);
 });
-const faqQuestions=document.querySelectorAll('question');
+const faqQuestions=document.querySelectorAll('.question');
 faqQuestions.forEach(function(item){
     item.addEventListener('click', function(){
         item.classList.toggle('active');
